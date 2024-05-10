b5fb5771c66da83ab99cef56a320ad656a4dd038
Mucahit Ugur <mchtugr@gmail.com>
2024-05-11 01:05:55 +0300

    feat: adds react native icons for tab bar icons

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 import Home from '../screens/Home/Home'
 import Profile from '../screens/Profile/Profile'
@@ -16,9 +17,23 @@ function Navigation(): React.JSX.Element {
       <NavigationContainer ref={navigationRef}>
         <BottomTab.Navigator
           initialRouteName={routes.home}
-          screenOptions={() => ({
+          screenOptions={({ route }) => ({
             headerShown: false,
             tabBarHideOnKeyboard: true,
+            tabBarIcon: ({ color }) => {
+              let iconName: string
+              switch (route.name) {
+                case routes.home:
+                  iconName = 'home'
+                  break
+                case routes.profile:
+                  iconName = 'user-alt'
+                  break
+                default:
+                  iconName = ''
+              }
+              return <Icon name={iconName} size={20} color={color} />
+            },
           })}>
           <BottomTab.Screen name={routes.home} component={Home} />
           <BottomTab.Screen name={routes.profile} component={Profile} />
