1c64f80dc6efd50892d2eaa2c76ed9d613c60b1a
Mucahit Ugur <mchtugr@gmail.com>
2024-05-14 17:44:05 +0300

    feat: adds redux as global state management

--- src/screens/Auth/SignIn/SignIn.tsx
+++ src/screens/Auth/SignIn/SignIn.tsx
@@ -1,6 +1,8 @@
 import { Button } from 'native-base'
 import * as React from 'react'
 import { Text, View } from 'react-native'
+import { useSelector } from 'react-redux'
+
 import { routes } from '../../../navigation/routes.const'
 
 interface SignInProps {
@@ -8,9 +10,10 @@ interface SignInProps {
 }
 
 const SignIn = (props: SignInProps) => {
+  const user = useSelector((state: any) => state.user)
   return (
     <View>
-      <Text>Sign In Page</Text>
+      <Text>Welcome {user.name}</Text>
       <Button onPress={() => props.navigation.navigate(routes.signUp)}>
         SignUp
       </Button>
