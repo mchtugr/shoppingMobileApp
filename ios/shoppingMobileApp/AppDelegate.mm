4389539bccfb212fbaf815c271e6cac63246e7c4
Mucahit Ugur <mchtugr@gmail.com>
2024-05-27 22:36:17 +0300

    feat: adds react native maps package for ios

--- ios/shoppingMobileApp/AppDelegate.mm
+++ ios/shoppingMobileApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"API_KEY"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"shoppingMobileApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,35 @@
-import { Text, View } from 'native-base'
+import { View } from 'native-base'
 import React from 'react'
+import { StyleSheet } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+})
 
 function Home(): React.JSX.Element {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 41.0082,
+            longitude: 28.9784,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}></MapView>
+      </View>
     </View>
   )
 }
