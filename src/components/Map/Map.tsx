4e04702dd53483328663aa0e18b1798f26626827
Mucahit Ugur <mchtugr@gmail.com>
2024-06-09 16:11:03 +0300

    feat: displays short address above marker from geocoding api

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,8 @@
+import { MAPS_API_KEY } from '@env'
+import axios from 'axios'
 import { Box } from 'native-base'
 import * as React from 'react'
+import { useState } from 'react'
 import {
   Animated,
   AnimatedRegion,
@@ -7,6 +10,8 @@ import {
   PROVIDER_GOOGLE,
 } from 'react-native-maps'
 
+import { Coordinate } from '~/types/address'
+import LocationMarker from '../address/LocationMarker'
 import styles from './Map.styles'
 
 interface MapProps {
@@ -19,11 +24,45 @@ interface MapProps {
 }
 
 const Map = ({ coordinate, hasFixedMarker, isAnimated }: MapProps) => {
+  const [coord, setCoord] = useState<Coordinate>(coordinate)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [address, setAddress] = useState<string>('')
   const region = new AnimatedRegion({
     ...coordinate,
     latitudeDelta: 0.003,
     longitudeDelta: 0.003,
   })
+
+  const getAddressFromCoordinate = async (c: Coordinate): Promise<void> => {
+    try {
+      setIsLoading(true)
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${c?.latitude},${c?.longitude}&key=${MAPS_API_KEY}`,
+      )
+
+      setAddress(
+        response.data.results[0].formatted_address
+          .split(',')
+          .slice(0, 2)
+          .join(','),
+      )
+    } catch (error) {
+      console.log('====================================')
+      console.log(error)
+      console.log('====================================')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const onRegionChange = (): void => {
+    setIsLoading(true)
+  }
+
+  const onRegionChangeComplete = async (c: Coordinate): Promise<void> => {
+    setCoord(c)
+    await getAddressFromCoordinate(c)
+  }
 
   return (
     <Box
@@ -39,9 +78,14 @@ const Map = ({ coordinate, hasFixedMarker, isAnimated }: MapProps) => {
         zoomEnabled={!!isAnimated}
         scrollEnabled={!!isAnimated}
         showsMyLocationButton={!!isAnimated}
+        onRegionChangeComplete={onRegionChangeComplete}
+        onRegionChange={onRegionChange}
         style={styles.map}>
-        {hasFixedMarker && <Marker coordinate={coordinate} />}
+        {hasFixedMarker && <Marker coordinate={coord} />}
       </Animated>
+      {!hasFixedMarker && (
+        <LocationMarker isLoading={isLoading} address={address} />
+      )}
     </Box>
   )
 }
--- src/screens/Home/AddAddress/AddAddress.tsx
+++ src/screens/Home/AddAddress/AddAddress.tsx
@@ -1,5 +1,6 @@
 import { View } from 'native-base'
 import React from 'react'
+import Map from '~/components/Map'
 import RouteHeader from '~/components/RouteHeader/RouteHeader'
 import { HomeStackRoutes } from '~/navigation/types'
 
@@ -13,9 +14,17 @@ const AddAddress = ({ navigation }: AddAddressProps) => {
   const onNavigateHome = () => {
     navigation.navigate(HomeStackRoutes.Home)
   }
+
   return (
     <View style={styles.container}>
       <RouteHeader title="Add New Address" onClose={onNavigateHome} />
+      <Map
+        isAnimated
+        coordinate={{
+          latitude: 40.98815406447282,
+          longitude: 29.02872167269661,
+        }}
+      />
     </View>
   )
 }
